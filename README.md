e824d4205aa10461609ad8a0888c6aa6ae96376d
alibaltaci <alibaltaci13@gmail.com>
2022-07-08 20:38:50 +0300

    installation / first styled comp.

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 https://styled-components.com/docs/basics
 
+# Motivation
+
+* React ve React Native componetlerimizi stillendirmek için yazılmış bir kütüphanedir.
+
+* **Automatic critical CSS:** Render edilen componentlerin bir listesini tutar ve bunların styleları ile birlikte gelmesini ya da silinirse styleları ile birlikte silinmesini sağlar. Yüklenmesi gereken kodları yükler (code spliting). İşi olmayan CSS ler yüklenmez.
+
+* **No Class Name Bugs:** Style Components, stilleriniz için benzersiz sınıf adları oluşturur. Çoğaltma, çakışma veya yazım hataları konusunda asla endişelenmenize gerek yok.
+
+* **Easier Deletion of CSS:** Bileşen kullanılmıyorsa (hangi takım algılayabilir) ve silinirse, tüm stilleri onunla birlikte silinir. 
+
+* **Simple Dynamic Styling.** Styled Components prop alabilir ve bunlara bir theme geçebiliriz. Alınan sınıfa göre stil belirlenir. Düzinelerce sınıfı manuel olarak yönetmek zorunda kalmadan basit ve sezgiseldir.
+
+* **Painless Maintenance:** (Sorunsuz Bakım) Componentler ve stiller birbirine bağlı. Bileşeninizi etkileyen stili bulmak için asla farklı dosyalar arasında arama yapmanız gerekmez, bu nedenle kod tabanınız ne kadar büyük olursa olsun bakım çocuk oyuncağıdır.
+
+* **Automatic Vendor Prefixing:** CSS'nizi mevcut standarda yazın ve gerisini stil bileşenlerinin halletmesine izin verin.
+
+# Installation
+
+```
+# with npm
+npm install --save styled-components
+
+# with yarn
+yarn add styled-components
+```
+
+` import Styled from "styled-components"; `
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import styled from 'styled-components';
+
+// Styled Components (CSS in JS and the CSS Module)
+// React & React Native 
+// Tagged Template Literals
+
+
+// Aşağıdaki Stylesheets ile yapılan işlemlerin aynısı Styled Componet ile yapalım
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <h1 className='header'>Styled Components</h1>
+//       <p className='content'>Code With Me!</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// Styled Components
+
+// Container
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 90rem;
+  height: 100vh;
+  padding: 2rem;
+  background-color: darkturquoise;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = styled.h1`
+  font-family: "Hachi Maru Pop", cursive;
+  font-size: 5rem;
+  color: white;
+`;
+
+const Content = styled.p`
+  color: white;
+  font-size: 3rem;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>Styled Components</Header>
+      <Content>Code With Me!</Content>
+    </Container>
+
   );
 }
 
 export default App;
+
